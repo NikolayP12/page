43512081f884a4575d33ebf682ce2f8a6b1853a4
NIKOLAY PETROV NIKOLOV <Nikolay.Petrov@alu.uclm.es>
2024-04-24 03:13:10 +0200

    - Se ha corregido el version.php

--- version.php
+++ version.php
@@ -17,14 +17,16 @@
 /**
  * Page module version information
  *
- * @package mod_page
+ * @package    mod_page
+ * @copyright  2009 Petr Skoda (http://skodak.org/)
  * @copyright  2009 Petr Skoda (http://skodak.org)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023100900;       // The current module version (Date: YYYYMMDDXX).
+$plugin->component = 'mod_page';    // Full name of the plugin (used for diagnostics).
+$plugin->release   = '1.3.4';       // Version release of the plugin.
+$plugin->version   = 2023100900;    // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;    // Requires this Moodle version.
-$plugin->component = 'mod_page';       // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
